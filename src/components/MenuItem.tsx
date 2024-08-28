b45ce79e3249243dfa2338c6f4633ec33de51b35
toba456 <tobias.marroquin@gmail.com>
2024-08-28 19:53:20 -0300

    creating hook useCard with addItem function

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -3,11 +3,15 @@ import type { MenuItem } from "../types";
 
 type MenuItemsProps = {
   item: MenuItem;
+  addItem: (item: MenuItem) => void;
 };
 
-export default function MenuItem({ item }: MenuItemsProps) {
+export default function MenuItem({ item, addItem }: MenuItemsProps) {
   return (
-    <button className=" border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between">
+    <button
+      className=" border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+      onClick={() => addItem(item)}
+    >
       <p>{item.name}</p>
       <p className=" font-black">${item.price}</p>
     </button>
