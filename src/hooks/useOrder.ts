3dccecd2340b3f0d6f16d8824a421b418d3b4a7d
toba456 <tobias.marroquin@gmail.com>
2024-09-04 01:21:28 -0300

    save order in a local storage

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -2,7 +2,12 @@ import { useState } from "react";
 import type { MenuItem, OrderItem } from "../types";
 
 export default function useOrder() {
-  const [order, setOrder] = useState<OrderItem[]>([]);
+  const initialOrder = (): OrderItem[] => {
+    const localStorageOrder = localStorage.getItem("order");
+    return localStorageOrder ? JSON.parse(localStorageOrder) : [];
+  };
+
+  const [order, setOrder] = useState<OrderItem[]>(initialOrder);
   const [tip, setTip] = useState(0);
 
   const addItem = (item: MenuItem) => {
@@ -14,18 +19,21 @@ export default function useOrder() {
           : orderItem
       );
       setOrder(updatedOrder);
+      localStorage.setItem("order", JSON.stringify(updatedOrder));
     } else {
       const newItem = {
         ...item,
         quantity: 1,
       };
       setOrder([...order, newItem]);
+      localStorage.setItem("order", JSON.stringify([...order, newItem]));
     }
   };
 
   const removeItem = (id: MenuItem["id"]) => {
     const updatedOrder = order.filter((orderItem) => orderItem.id !== id);
     setOrder(updatedOrder);
+    localStorage.setItem("order", JSON.stringify(updatedOrder));
   };
 
   const placeOrder = () => {
