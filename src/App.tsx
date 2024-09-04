90890ac0121af0c836654a7dd436d1e9ac090b26
toba456 <tobias.marroquin@gmail.com>
2024-09-04 01:58:51 -0300

    adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ import useOrder from "./hooks/useOrder";
 function App() {
   const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder();
   return (
-    <>
-      <header className=" bg-teal-400 py-5">
-        <h1 className=" text-center text-4xl font-black">
+    <div className="bg-[url('/src/assets/img/backgroundImage.jpg')]">
+      <header className="py-5">
+        <h1 className=" text-center text-4xl font-mono ">
           Calculadora de Propinas y Consumo
         </h1>
       </header>
-      <main className="  max-w-7xl mx-auto py-20 grid  md:grid-cols-2">
+      <main className=" smax-w-7xl mx-auto px-5 py-20 grid  md:grid-cols-2 ">
         <div className=" p-5">
           <h2 className=" text-4xl font-black">Menu</h2>
           <div className=" space-y-3 mt-10">
@@ -23,7 +23,7 @@ function App() {
             ))}
           </div>
         </div>
-        <div className=" border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+        <div className=" border border-dashed border-slate-400 bg-slate-200 bg-opacity-40 p-5 rounded-lg space-y-10">
           {order.length > 0 ? (
             <>
               <OrderContens order={order} removeItem={removeItem} />
@@ -31,11 +31,11 @@ function App() {
               <OrderTotals order={order} tip={tip} placeOrder={placeOrder} />
             </>
           ) : (
-            <p className="text-center">La órden está vacía</p>
+            <p className="text-center text-xl">La órden está vacía</p>
           )}
         </div>
       </main>
-    </>
+    </div>
   );
 }
 
