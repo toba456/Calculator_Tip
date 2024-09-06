840a1b30d1405e76bb6d40b0293993cb424afa77
toba456 <tobias.marroquin@gmail.com>
2024-09-06 11:42:53 -0300

    code cleaning

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { MenuItem } from "../types";
 
 type MenuItemsProps = {
--- src/components/OrderContens.tsx
+++ src/components/OrderContens.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { MenuItem, OrderItem } from "../types";
 import { formatCurrency } from "../helpers";
 
